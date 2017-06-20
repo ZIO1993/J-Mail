99c4aaa219fb035ad88a8c2489ded2d8c3ae3aef
ZIO1993 <aldodintrono93@gmail.com>
2017-06-20 15:45:19 +0200

    Miglioramenti vari.

--- src/rubrica/Rubrica.java
+++ src/rubrica/Rubrica.java
@@ -1,8 +1,10 @@
 package rubrica;
 
+import javax.swing.*;
 import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by ALDO on 20/06/2017.
@@ -14,18 +16,51 @@ import java.util.List;
 public class Rubrica {
     private List<Persona> persone;
     private File file;
+    private String nomeFile = "Rubrica.txt";
+    private String formato;
 
-    public Rubrica(String nomeFile){
+    public Rubrica(File f){
         persone = new LinkedList<Persona>();
-        file = new File(nomeFile);
+        if(f!=null){
+            file=f;
+        }else{
+            file = new File(nomeFile);
+        }
         caricaRubrica();
     }
 
     private void caricaRubrica(){
-
+        try {
+            Scanner scanner = new Scanner(file);
+            if(!scanner.hasNext()){
+                throw new Exception("vuoto");
+            }
+            formato = scanner.nextLine();
+            String temp;
+            Persona p;
+            while(scanner.hasNext()){
+                temp = scanner.nextLine();
+                p = getPersonaByFormato(formato, temp);
+                if(p!=null && !persone.contains(p))
+                    persone.add(p);
+            }
+        }catch (Exception e){
+            if(e.getMessage().equals("vuoto") && !file.getName().equals(nomeFile)){
+                JOptionPane.showMessageDialog(null, "Il file scelto è vuoto!", "Errore", JOptionPane.ERROR_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(null, "Errore nel caricamento della rubrica!", "Errore", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
-    private void salvaRubrica(){
+    private Persona getPersonaByFormato(String formato, String riga){
+        Persona ret = null;
+        String separatore = formato.substring(0,1);
+
+        return ret;
+    }
+
+    public void salvaRubrica(){
 
     }
 
